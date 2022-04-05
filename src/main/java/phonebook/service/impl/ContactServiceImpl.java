b5b14d966da3f8ff896301ef76772fee6f960c6d
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-04-05 14:28:39 +0400

    final update

--- src/main/java/phonebook/service/impl/ContactServiceImpl.java
+++ src/main/java/phonebook/service/impl/ContactServiceImpl.java
@@ -285,7 +285,7 @@ public class ContactServiceImpl implements ContactService {
     }
 
     public static void SystemExit() {
-        System.out.println("\u001B[39m" + "phone book is closed");
+        System.out.println("\u001B[36m" + "phone book is closed");
         System.out.println("Thank you for using our phonebook ☺");
         System.out.println("Phone Book version 1.2");
         System.exit(0);
